851700e24625e291ef02b0ee2db270b4623b46dd
wk <somnuk.wk@gmail.com>
2020-04-15 23:25:47 +0700

    JObject

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ curl http://localhost:5000/api/hello/annonymous
 
 # The collection type 'System.Collections.Generic.Dictionary`2[System.Int32,System.Object]' is not supported.
 curl http://localhost:5000/api/hello/int
+curl http://localhost:5000/api/hello/jobj
+curl http://localhost:5000/api/hello/dict
 ```
 
 # R
--- src/MyWeb/Controllers/HelloController.cs
+++ src/MyWeb/Controllers/HelloController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace MyWeb.Controllers {
     [ApiController]
@@ -26,12 +27,41 @@ namespace MyWeb.Controllers {
 
         [HttpGet]
         public Dictionary<int, object> Int() {
+            // Error
             return new Dictionary<int, object> {
                 [1] = 100,
                 [2] = new {
                     B1 = 100,
                     B2 = 100
                 }
+            };
+        }
+
+        [HttpGet]
+        public object Jobj() {
+            var j = new JObject();
+            j.Add("a", 100);
+            j.Add("b", 200);
+
+            var jj = j.ToObject<Dictionary<string, object>>();
+
+            return new {
+                A = 100,
+                J = jj
+            };
+        }
+
+        [HttpGet]
+        public object Dict() {
+            // Error
+
+            var j = new JObject();
+            j.Add("a", 100);
+            j.Add("b", 200);
+
+            return new {
+                A = 100,
+                J = j
             };
         }
     }
